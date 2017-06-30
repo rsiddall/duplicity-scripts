d943b573c453f5933226c7dbde858fe0ba0fcb54
Richard Siddall <richard.siddall@elirion.net>
2017-06-30 14:10:47 -0400

    Added support for the file globbing list, and optional GPG usage, same as in the backup script.

--- duplicity-verify.sh
+++ duplicity-verify.sh
@@ -7,8 +7,15 @@ export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
 export PASSPHRASE
 
-duplicity verify -v4 ${DEST} ${SOURCE} \
-	--include-globbing-filelist=/usr/local/etc/duplicity-backup.list
+    if [ "${GPG_KEY}x" != "x" ]; then
+        GPG_SWITCH="--encrypt-key=${GPG_KEY} --sign-key=${GPG_KEY}"
+    else
+        GPG_SWITCH="--no-encryption"
+    fi
+
+duplicity verify -v4 --include-globbing-filelist=/usr/local/etc/duplicity-backup.list \
+        ${GPG_SWITCH} \
+	${DEST} ${SOURCE}
 
 # Reset the ENV variables. Don't need them sitting around
 unset AWS_ACCESS_KEY_ID
