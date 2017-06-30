955a6b15f680d6506cff1db40a627e4faf3a3a37
Richard Siddall <richard.siddall@elirion.net>
2017-06-30 11:40:41 -0400

    Move the configuration variables into /usr/local/etc/duplicity-backup.cfg, and the list of files to verify into /usr/local/etc/duplicity-backup.list

--- duplicity-verify.sh
+++ duplicity-verify.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
+
+source "/usr/local/etc/duplicity-backup.cfg"
+
 # Export some ENV variables so you don't have to type anything
-export AWS_ACCESS_KEY_ID="IAM_USER_ACCESS_KEY_ID"
-export AWS_SECRET_ACCESS_KEY="IAM_USER_SECRET_ACCESS_KEY"
-export PASSPHRASE="GPG_OR_SOME_OTHER_PASSPHRASE"
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
+export PASSPHRASE
 
-# The S3 destination followed by bucket name
-DEST="s3://s3.amazonaws.com/example.com/"
-
-# The source of your backup
-SOURCE=/
-# Your GPG key
-#GPG_KEY=YOUR_GPG_KEY
-
-duplicity verify -v4 ${DEST} ${SOURCE}\
-    --include=/var/www \
-    --include=/etc \
-    --include=/home \
-    --include=/root \
-    --exclude=/** \
-    --exclude=/root/.cache 
+duplicity verify -v4 ${DEST} ${SOURCE} \
+	--include-globbing-filelist=/usr/local/etc/duplicity-backup.list
 
 # Reset the ENV variables. Don't need them sitting around
 unset AWS_ACCESS_KEY_ID
